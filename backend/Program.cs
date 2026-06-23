77dad082aae28b3879d54469428f5cb1ec2900d2
Matt Musiel <80542428+mmusiel@users.noreply.github.com>
2026-06-23 07:15:54 -0500

    wip: save feature/api-integration work

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,10 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy => {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+})
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.MapGet("/api/diagnostics/bolt", () =>
 {
     var myBolt = new Bolt("Titanium", 2.0, 0.8, "A9A9A9");
-
     return myBolt;
 });
 
